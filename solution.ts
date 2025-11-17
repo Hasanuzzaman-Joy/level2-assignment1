1b723363fc274fae52478f63c08f7a2c0a4cca5d
TheStackMaster <unijoy84@gmail.com>
2025-11-18 05:19:03 +0600

    solve problem 5

--- solution.ts
+++ solution.ts
@@ -1,51 +1,73 @@
+type Primitive = string | number | boolean;
 
-type Primitive_1 = string | number | boolean;
-
-function formatValue(value:Primitive_1):Primitive_1{
-    if(typeof value === 'string'){
-        return value.toUpperCase();
-    }
-    else if(typeof value === 'number'){
-        return value*10;
-    }
-    else{
-        return !value;
-    }
+function formatValue(value: Primitive): Primitive {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
 }
 
 
-
+// 2nd Task
 
 type GetLength = string | any[];
 
-function getLength(value: GetLength):number{
-    if(typeof value === 'string'){
-        return value.length;
-    }
-    else if(Array.isArray(value)){
-        return value.length;
-    }
-    return 0;
+function getLength(value: GetLength): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
 }
 
 
 
 
 
-
-
+//  3rd Task
 
 class Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 
-    constructor(name:string, age:number){
-        this.name = name;
-        this.age = age;
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-    getDetails():string{
-        return `'Name: ${this.name}, Age: ${this.age}'`;
-    }
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
 }
 
+
+
+
+// 4th Task
+
+type ObjectsType = {
+  title: string;
+  rating: number;
+};
+
+type ArrayOfObjects = Array<ObjectsType>;
+
+function filterByRating(arr: ArrayOfObjects): ArrayOfObjects {
+  const newArr: ArrayOfObjects = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].rating >= 4 && arr[i].rating <= 5) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+
+
+
+// 5th Task
