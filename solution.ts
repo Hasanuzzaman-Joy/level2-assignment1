b2f3869932dac098278669e97189411913a8920a
TheStackMaster <unijoy84@gmail.com>
2025-11-18 08:58:09 +0600

    Add readme file

--- solution.ts
+++ solution.ts
@@ -10,7 +10,12 @@ function formatValue(value: Primitive): Primitive {
   }
 }
 
-// 2nd Task
+
+
+
+
+
+
 
 type GetLength = string | any[];
 
@@ -23,7 +28,12 @@ function getLength(value: GetLength): number {
   return 0;
 }
 
-//  3rd Task
+
+
+
+
+
+
 
 class Person {
   name: string;
@@ -39,7 +49,13 @@ class Person {
   }
 }
 
-// 4th Task
+
+
+
+
+
+
+
 
 type ObjectsType = {
   title: string;
@@ -58,6 +74,9 @@ function filterByRating(arr: ArrayOfObjects): ArrayOfObjects {
   }
   return newArr;
 }
+
+
+
 
 // 5th Task
 
@@ -89,4 +108,67 @@ function printBookDetails(book: Book) {
       book.publishedYear
     }, Available: ${book.isAvailable ? "Yes" : "No"}`
   );
+}
+
+// 7th Task
+
+type ArrayValues = string | number;
+
+function getUniqueValues(
+  arr1: ArrayValues[],
+  arr2: ArrayValues[]
+): ArrayValues[] {
+  const combinedLength = arr1.length + arr2.length;
+  const combinedArray: ArrayValues[] = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    combinedArray[i] = arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    combinedArray[arr1.length + i] = arr2[i];
+  }
+
+  const uniqueArray: ArrayValues[] = [];
+  let uniqueCount = 0;
+
+  for (let i = 0; i < combinedLength; i++) {
+    let isDuplicate = false;
+    for (let j = 0; j < uniqueCount; j++) {
+      if (combinedArray[i] === uniqueArray[j]) {
+        isDuplicate = true;
+        break;
+      }
+    }
+    if (!isDuplicate) {
+      uniqueArray[uniqueCount] = combinedArray[i];
+      uniqueCount++;
+    }
+  }
+
+  const result: ArrayValues[] = [];
+  for (let i = 0; i < uniqueCount; i++) {
+    result[i] = uniqueArray[i];
+  }
+
+  return result;
+}
+
+// 8th Task
+
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+}
+
+type ArrayOfProducts = Product[];
+
+function calculateTotalPrice(products: ArrayOfProducts): number {
+  const total = products.map((product) => {
+    const discount = product.discount ?? 0;
+    const priceAfterDiscount = product.price * (1 - discount / 100);
+    return priceAfterDiscount * product.quantity;
+  });
+  return total.reduce((initial, productTotal) => initial + productTotal, 0);
 }
